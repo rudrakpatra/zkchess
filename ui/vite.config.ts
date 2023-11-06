f3342db0cbea2d9721f3aaf7f9a4cc8c665fbe50
rudrakpatra <rudrakpatracodes@gmail.com>
2023-11-06 22:45:17 +0530

    added configure preview server

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,13 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, type ViteDevServer } from 'vite';
+import { defineConfig, type ViteDevServer, type PreviewServerForHook } from 'vite';
 
 /** @type {import('vite').Plugin} */
 const viteServerConfig = {
-	name: 'add headers',
+	name: 'cross-origin-isolation',
 	configureServer: (server: ViteDevServer) => {
-		server.middlewares.use((req, res, next) => {
-			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+		server.middlewares.use((_, response, next) => {
+			response.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+			response.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+			next();
+		});
+	},
+	configurePreviewServer: (server: PreviewServerForHook) => {
+		server.middlewares.use((_, response, next) => {
+			response.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+			response.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
 			next();
 		});
 	}
