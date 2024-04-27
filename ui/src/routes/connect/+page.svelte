09142700c7297f08d123a9d90844a6ada07412cc
rudrakpatra <rudrakpatracodes@gmail.com>
2024-04-28 02:15:09 +0530

    connect page

--- ui/src/routes/connect/+page.svelte
+++ ui/src/routes/connect/+page.svelte
@@ -3,13 +3,23 @@
 	import RippleButton from '$lib/components/general/RippleButton.svelte';
 	import ellipsis from '$lib/ellipsis';
 </script>
-
-<AuroConnect let:connect>
-	<RippleButton on:click={connect}>
-			{#if $publicKey}
-			{ellipsis($publicKey, 12)}
-			{:else}
-			Connect
-			{/if}
-	</RippleButton>
-</AuroConnect>
+<main>
+	<div class="flex flex-col justify-center">
+		<AuroConnect let:connect>
+			<RippleButton on:click={connect}>
+				{#if $publicKey}
+				{ellipsis($publicKey, 12)}
+				{:else}
+				Connect
+				{/if}
+			</RippleButton>
+		</AuroConnect>
+	</div>
+</main>
+<style lang="scss">
+	main {
+		@apply bg-background fixed inset-0  grid justify-center overflow-y-scroll p-2;
+		@apply -top-1 pt-3; //prevent browser address bar from sliding up
+		container-type: inline-size;
+	}
+</style>
