f10ab44caba7e59be547db814b7e24ba27f8dbb5
rudrakpatra <rudrakpatracodes@gmail.com>
2024-04-29 14:35:18 +0530

    matchmaker fix

--- matchmaker/src/routes/home.ts
+++ matchmaker/src/routes/home.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express'
 
 const router = Router()
 router.get('/', (req: Request, res: Response) => {
-  res.json({ route: 'this is the home route' })
+  res.status(200).send('health check')
 })
 
 export default router
