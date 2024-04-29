d043b2f42bd34172a23db7dc2d54c983a4bb7e9b
rudrakpatra <rudrakpatracodes@gmail.com>
2024-04-29 16:55:43 +0530

    added matchmaker

--- matchmaker/src/main.ts
+++ matchmaker/src/main.ts
@@ -1,6 +1,7 @@
 import express, { Application, Response, Request, NextFunction } from 'express'
 import env from 'dotenv'
 import { Home } from './routes'
+import cors from 'cors'
 
 //.env
 env.config()
@@ -10,7 +11,7 @@ const app: Application = express()
 
 const PORT = process.env.PORT || 1337
 const SERVER_URL = process.env.SERVER_URL || 'http://localhost'
-
+app.use(cors())
 app.use('/', Home)
 
 app.listen(PORT, () => {
