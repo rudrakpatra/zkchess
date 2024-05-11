fac6419c8008db8c6e9ca6f7843fb817ccf159ef
rudrakpatra <rudrakpatracodes@gmail.com>
2024-05-11 18:07:12 +0530

    corrected verboseLogger

--- ui/src/lib/VerboseLogger.ts
+++ ui/src/lib/VerboseLogger.ts
@@ -1,5 +1,12 @@
 const getLogger = (verboseLvl: number) => {
-	console.log(`Using Verbose Logger`, 'color:orangered;');
-	return (data: any[], lvl: number) => lvl >= verboseLvl && console.log(...data);
+	console.log(
+		'%c[verbose]\n\n',
+		'color:#a63; text-decoration: underline;',
+		'using verbose level',
+		verboseLvl
+	);
+	return (data: any[], lvl: number) =>
+		lvl <= verboseLvl &&
+		console.log('%c[verbose]\n\n', 'color:#a63; text-decoration: underline;', ...data);
 };
 export default getLogger;
